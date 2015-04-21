e967bda7a51afc8a6e6b3f3ee65e6214aec75b3a
MaxRoecker <max.roecker@gmail.com>
2015-04-21 15:34:43 -0300

    Definições

--- Projeto/color.h
+++ Projeto/color.h
@@ -37,6 +37,10 @@
 #define STOP_CONV	4
 #define STOP_ALG	5 // specific for some algorithms
 
+/* ALTERADO POR JOAO LUIZ E MAX */
+#define BUFFER_SIZE 10
+/*FIM DA ALTERACAO*/
+
 struct gcp_t {
   int nof_vertices;		// number of vertices
   int nof_edges;			// number of edges
